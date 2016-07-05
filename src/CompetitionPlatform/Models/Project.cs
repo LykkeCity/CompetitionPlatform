9e901369a4985147fe5aa78d99bbfbaa6564d1c9
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-07-05 23:14:13 +0400

    Added Project entity.

---
+++ src/CompetitionPlatform/Models/Project.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompetitionPlatform.Models
+{
+    public class Project
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Status Status { get; set; }
+        public List<string> Categories { get; set; }
+        public DateTime CompetitionRegistrationDeadline { get; set; }
+        public DateTime ImplementationDeadline { get; set; }
+        public DateTime VotingDeadline { get; set; }
+        public List<double> Budget { get; set; }
+        public int VotesFor { get; set; }
+        public int VotesAgainst { get; set; }
+    }
+}
---
+++ src/CompetitionPlatform/Models/Status.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompetitionPlatform.Models
+{
+    public enum Status
+    {
+        Initiative,
+        CompetitionRegistration,
+        Implementation,
+        Voting,
+        Archive
+    }
+}
