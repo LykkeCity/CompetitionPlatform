39212cfb9562d3577135e5afc115170402bb9081
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-07-07 19:48:01 +0400

    Split Budget into separate properties, added validation and display names.

--- src/CompetitionPlatform/Models/Project.cs
+++ src/CompetitionPlatform/Models/Project.cs
@@ -11,19 +11,42 @@ namespace CompetitionPlatform.Models
         [Required]
         [Display(Name = "Project name")]
         public string Name { get; set; }
+
         [Required]
         [Display(Name = "Describe the project")]
         public string Description { get; set; }
+
         public Status Status { get; set; }
+
         public List<string> Categories { get; set; }
+
+        [Required]
         [Display(Name = "Competition Registration")]
         public DateTime CompetitionRegistrationDeadline { get; set; }
+
+        [Required]
         [Display(Name = "Implementation")]
         public DateTime ImplementationDeadline { get; set; }
+
+        [Required]
         [Display(Name = "Voting")]
         public DateTime VotingDeadline { get; set; }
-        public List<double> Budget { get; set; }
+
+        [Required]
+        [Display(Name = "1ST place")]
+        public double BudgetFirstPlace { get; set; }
+
+
+        [Display(Name = "2ND place")]
+        public double? BudgetSecondPlace { get; set; }
+
+
+        [Display(Name = "3D place")]
+        public double? BudgetThirdPlace { get; set; }
+
         public int VotesFor { get; set; }
+
         public int VotesAgainst { get; set; }
+
     }
 }
