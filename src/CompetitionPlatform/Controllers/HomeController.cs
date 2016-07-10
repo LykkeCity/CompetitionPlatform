8d598f9d31f867ca46cd9cd17bbbf12634a2958d
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-07-10 13:59:43 +0400

    Added code to get all projects in the controller and a view to display them.

--- src/CompetitionPlatform/Controllers/HomeController.cs
+++ src/CompetitionPlatform/Controllers/HomeController.cs
@@ -2,15 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CompetitionPlatform.Data.AzureRepositories.Project;
 using Microsoft.AspNetCore.Mvc;
+using CompetitionPlatform.Models.ProjectViewModels;
 
 namespace CompetitionPlatform.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IProjectRepository _projectRepository;
+
+        public HomeController(IProjectRepository projectRepository)
         {
-            return View();
+            _projectRepository = projectRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var projects = await _projectRepository.GetProjectsAsync();
+
+            var compactModels = new List<ProjectCompactViewModel>();
+
+            foreach (var project in projects)
+            {
+                var compactModel = new ProjectCompactViewModel()
+                {
+                    Name = project.Name.Length > 35? project.Name.Substring(0,32) + "..." : project.Name,
+                    Description = project.Description.Length > 500 ? project.Description.Substring(0, 497) + "..." : project.Description,
+                    Status = project.Status,
+                    BudgetFirstPlace = project.BudgetFirstPlace,
+                    VotesFor = project.VotesFor,
+                    VotesAgainst = project.VotesAgainst
+                };
+
+                compactModels.Add(compactModel);
+            }
+
+            var viewModel = new ProjectListIndexViewModel()
+            {
+                Projects = compactModels
+            };
+
+            return View(viewModel);
         }
 
         public IActionResult About()
