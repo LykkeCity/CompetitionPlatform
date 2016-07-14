379ec4ee25b3b1be64a1b1777edad2a18a0da20e
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-07-14 22:14:00 +0400

    Added controller and Models for user management.

---
+++ src/CompetitionPlatform/Controllers/UsersManagementController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CompetitionPlatform.Data.AzureRepositories.Users;
+using CompetitionPlatform.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompetitionPlatform.Controllers
+{
+    public class UsersManagementController : Controller
+    {
+        private readonly IUsersRepository _usersRepository;
+
+        public UsersManagementController(IUsersRepository usersRepository)
+        {
+            _usersRepository = usersRepository;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateUser(EditUserModel model)
+        {
+                if (string.IsNullOrEmpty(model.FullName))
+                    return null;
+
+            await _usersRepository.SaveAsync(model, model.Password);
+
+            return View("~/Views/Account/Login.cshtml");
+        }
+    }
+}
---
+++ src/CompetitionPlatform/Models/UsersManagementModels.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using CompetitionPlatform.Data.AzureRepositories.Users;
+
+namespace CompetitionPlatform.Models
+{
+    public class EditUserModel : IUser
+    {
+        public string Create { get; set; }
+
+        [Required]
+        [Display(Name = "Username")]
+        public string Id { get; set; }
+
+        [Required]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        public string IsAdmin { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        bool IUser.IsAdmin => !string.IsNullOrEmpty(IsAdmin);
+    }
+
+    public class UserLoginModel
+    {
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
