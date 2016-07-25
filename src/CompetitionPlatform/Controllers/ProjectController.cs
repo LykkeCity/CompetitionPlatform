0547caa5e3a220c575c033ff6fadbdf9778c5530
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-07-25 21:42:09 +0400

    Showing newest comments first.

--- src/CompetitionPlatform/Controllers/ProjectController.cs
+++ src/CompetitionPlatform/Controllers/ProjectController.cs
@@ -56,6 +56,8 @@ namespace CompetitionPlatform.Controllers
             var project = await _projectRepository.GetAsync(id);
 
             var comments = await _projectCommentsRepository.GetProjectCommentsAsync(id);
+            
+            comments = comments.OrderBy(c => c.Created).Reverse().ToList();
 
             var commentsPartial = new ProjectCommentPartialViewModel()
             {
