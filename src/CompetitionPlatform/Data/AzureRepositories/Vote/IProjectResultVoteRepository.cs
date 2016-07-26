e4b22dcb70408aa5cd96bff2fb70b33c7fa3112c
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-07-26 21:04:13 +0400

    Added repository and interface for Project Result Votes.

---
+++ src/CompetitionPlatform/Data/AzureRepositories/Vote/IProjectResultVoteRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompetitionPlatform.Data.AzureRepositories.Vote
+{
+    public interface IProjectResultVoteData
+    {
+        string ProjectId { get; set; }
+        string ParticipantUserId { get; set; }
+        string VoterUserId { get; set; }
+    }
+
+    public interface IProjectResultVoteRepository
+    {
+        Task SaveAsync(IProjectResultVoteData projectResultVoteData);
+        Task<IEnumerable<IProjectResultVoteData>> GetProjectResultVotesAsync(string projectId);
+    }
+}
---
+++ src/CompetitionPlatform/Data/AzureRepositories/Vote/ProjectResultVoteRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AzureStorage.Tables;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace CompetitionPlatform.Data.AzureRepositories.Vote
+{
+    public class ProjectResultVoteEntity : TableEntity, IProjectResultVoteData
+    {
+        public static string GeneratePartitionKey(string projectId)
+        {
+            return projectId;
+        }
+
+        public static string GenerateRowKey(string userId)
+        {
+            return userId;
+        }
+
+        public string ProjectId { get; set; }
+        public string ParticipantUserId { get; set; }
+        public string VoterUserId { get; set; }
+
+        public static ProjectResultVoteEntity Create(IProjectResultVoteData src)
+        {
+            var result = new ProjectResultVoteEntity
+            {
+                RowKey = GenerateRowKey(src.VoterUserId),
+                PartitionKey = GeneratePartitionKey(src.ProjectId),
+                ParticipantUserId = src.ParticipantUserId
+            };
+
+            return result;
+        }
+    }
+
+    public class ProjectResultVoteRepository : IProjectResultVoteRepository
+    {
+        private readonly IAzureTableStorage<ProjectResultVoteEntity> _projectResultVoteTableStorage;
+
+        public ProjectResultVoteRepository(IAzureTableStorage<ProjectResultVoteEntity> projectResultVoteTableStorage)
+        {
+            _projectResultVoteTableStorage = projectResultVoteTableStorage;
+        }
+
+        public async Task SaveAsync(IProjectResultVoteData projectResultVoteData)
+        {
+            var newEntity = ProjectResultVoteEntity.Create(projectResultVoteData);
+            await _projectResultVoteTableStorage.InsertAsync(newEntity);
+        }
+
+        public async Task<IEnumerable<IProjectResultVoteData>> GetProjectResultVotesAsync(string projectId)
+        {
+            var partitionKey = ProjectResultVoteEntity.GeneratePartitionKey(projectId);
+            return await _projectResultVoteTableStorage.GetDataAsync(partitionKey);
+        }
+    }
+}
