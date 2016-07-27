0fd231c652b63aad07b2ee11bba1fbeff5904f99
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-07-27 22:16:58 +0400

    Added interface and repository for Project Result files.

---
+++ src/CompetitionPlatform/Data/AzureRepositories/Result/IProjectResultRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompetitionPlatform.Data.AzureRepositories.Result
+{
+    public interface IProjectResultRepository
+    {
+        Task InsertProjectResult(Stream stream, string projectId, string userName);
+        Task<Stream> GetProjectResult(string projectId, string userName);
+    }
+}
---
+++ src/CompetitionPlatform/Data/AzureRepositories/Result/ProjectResultRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AzureStorage.Blobs;
+
+namespace CompetitionPlatform.Data.AzureRepositories.Result
+{
+    public class ProjectResultRepository : IProjectResultRepository
+    {
+        private readonly IAzureBlob _blobStorage;
+
+        private const string ContainerName = "projectresults";
+
+        public ProjectResultRepository(IAzureBlob blobStorage)
+        {
+            _blobStorage = blobStorage;
+        }
+
+        public async Task InsertProjectResult(Stream stream, string projectId, string userName)
+        {
+            await _blobStorage.SaveBlobAsync(ContainerName, projectId + userName, stream);
+        }
+
+        public async Task<Stream> GetProjectResult(string projectId, string userName)
+        {
+            return await _blobStorage.GetAsync(ContainerName, projectId + userName);
+        }
+    }
+}
