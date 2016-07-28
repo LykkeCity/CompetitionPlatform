004c67fbed9d00a388ff3530ca80554784b45db0
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-07-28 22:28:54 +0400

    Added interface and repository for Project Result Info.

---
+++ src/CompetitionPlatform/Data/AzureRepositories/Result/IProjectResultInfoRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CompetitionPlatform.Data.AzureRepositories.Result
+{
+    public interface IProjectResultInfoData
+    {
+        string Id { get; }
+        string ProjectId { get; set; }
+        string User { get; set; }
+        DateTime Submitted { get; set; }
+    }
+
+    public interface IProjectResultInfoRepository
+    {
+        Task<IEnumerable<IProjectResultInfoData>> GetResultsAsync(string resultId);
+        Task SaveAsync(IProjectResultInfoData resultInfoData);
+    }
+}
