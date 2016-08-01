a7bb4dd0a2de3ea6a053c7aa783c94aba472cac4
ozhelenkov <oleg.zhelenkov@hotmail.com>
2016-08-01 23:41:47 +0300

    patch-project-json.js corrections

--- build/patch-project-json.js
+++ build/patch-project-json.js
@@ -1,10 +1,22 @@
 var jsonfile = require('jsonfile');
 
-var file = '../src/competitionplatform/project.json';
+// Read in the file to be patched
+var file = process.argv[2]; // e.g. '../src/MyProject/project.json'
+if (!file)
+    console.log("No filename provided");
+console.log("File: " + file);
+
+// Read in the build version (this might be provided by the CI server)
+var version = process.argv[3]; // e.g. '1.0.42-beta'
+if (!version)
+    console.log("No version provided");
 
 jsonfile.readFile(file, function (err, project) {
-    project.version = process.env.APPVEYOR_BUILD_VERSION;
+    project.version = version;
     jsonfile.writeFile(file, project, {spaces: 2}, function(err) {
-        console.error(err);
+		if(err) {
+			return console.log(err);
+		}
+		console.log("The file was saved!");
     });
 })
