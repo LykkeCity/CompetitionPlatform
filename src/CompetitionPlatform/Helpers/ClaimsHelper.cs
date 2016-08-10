c2521ae1ad08b0adb255f33af6a6b9c41d62915e
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-08-10 14:35:56 +0400

    Add ClaimsHelper class for easier access to authenticated user information.

---
+++ src/CompetitionPlatform/Helpers/ClaimsHelper.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using CompetitionPlatform.Models;
+
+namespace CompetitionPlatform.Helpers
+{
+    public static class ClaimsHelper
+    {
+        public static CompetitionPlatformUser GetUser(IIdentity identity)
+        {
+            var claimsIdentity = (ClaimsIdentity) identity;
+            IEnumerable<Claim> claims = claimsIdentity.Claims;
+
+            var claimsList = claims as IList<Claim> ?? claims.ToList();
+
+            var firstName = claimsList.Where(c => c.Type == ClaimTypes.GivenName)
+                   .Select(c => c.Value).SingleOrDefault();
+
+            var lastName = claimsList.Where(c => c.Type == ClaimTypes.Surname)
+                   .Select(c => c.Value).SingleOrDefault();
+
+            var email = claimsList.Where(c => c.Type == ClaimTypes.Email)
+                   .Select(c => c.Value).SingleOrDefault();
+
+            var user = new CompetitionPlatformUser
+            {
+                Email = email,
+                FirstName = firstName,
+                LastName = lastName
+            };
+
+            return user;
+        }
+    }
+}
