02cff73f7a7bb54bde7e9fd9a062e2aa75ffcbf7
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-08-11 08:46:53 +0400

    Add interface and repository for project participants.

---
+++ src/CompetitionPlatform/Data/AzureRepositories/Users/IProjectParticipateRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompetitionPlatform.Data.AzureRepositories.Users
+{
+    public interface IProjectParticipateData
+    {
+        string UserId { get; set; }
+        string ProjectId { get; set; }
+    }
+
+    public interface IProjectParticipateRepository
+    {
+        Task SaveAsync(IProjectParticipateData projectParticipateData);
+        Task<IProjectParticipateData> GetAsync(string projectId, string userId);
+        Task<IEnumerable<IProjectParticipateData>> GetProjectParticipants(string projectId);
+        Task<IProjectParticipateData> DeleteAsync(string userId, string projectId);
+    }
+}
---
+++ src/CompetitionPlatform/Data/AzureRepositories/Users/ProjectParticipateRepository.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AzureStorage.Tables;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace CompetitionPlatform.Data.AzureRepositories.Users
+{
+    public class ProjectParticipateEntity : TableEntity, IProjectParticipateData
+    {
+        public static string GeneratePartitionKey(string projectId)
+        {
+            return projectId;
+        }
+
+        public static string GenerateRowKey(string userId)
+        {
+            return userId;
+        }
+
+        public static ProjectParticipateEntity Create(IProjectParticipateData src)
+        {
+            var result = new ProjectParticipateEntity
+            {
+                PartitionKey = GeneratePartitionKey(src.ProjectId),
+                RowKey = GenerateRowKey(src.UserId)
+            };
+
+            return result;
+        }
+
+        public string UserId { get; set; }
+        public string ProjectId { get; set; }
+    }
+    public class ProjectParticipateRepository : IProjectParticipateRepository
+    {
+        private readonly IAzureTableStorage<ProjectParticipateEntity> _projectParticipateTableStorage;
+
+        public ProjectParticipateRepository(IAzureTableStorage<ProjectParticipateEntity> projectParticipateTableStorage)
+        {
+            _projectParticipateTableStorage = projectParticipateTableStorage;
+        }
+
+        public async Task SaveAsync(IProjectParticipateData projectParticipateData)
+        {
+            var newEntity = ProjectParticipateEntity.Create(projectParticipateData);
+            await _projectParticipateTableStorage.InsertAsync(newEntity);
+        }
+
+        public async Task<IProjectParticipateData> GetAsync(string projectId, string userId)
+        {
+            var partitionKey = ProjectParticipateEntity.GeneratePartitionKey(projectId);
+            var rowKey = ProjectParticipateEntity.GenerateRowKey(userId);
+
+            return await _projectParticipateTableStorage.GetDataAsync(partitionKey, rowKey);
+        }
+
+        public async Task<IEnumerable<IProjectParticipateData>> GetProjectParticipants(string projectId)
+        {
+            var partitionKey = ProjectParticipateEntity.GeneratePartitionKey(projectId);
+
+            return await _projectParticipateTableStorage.GetDataAsync(partitionKey);
+        }
+
+        public async Task<IProjectParticipateData> DeleteAsync(string userId, string projectId)
+        {
+            var partitionKey = ProjectParticipateEntity.GeneratePartitionKey(projectId);
+            var rowKey = ProjectParticipateEntity.GenerateRowKey(userId);
+
+            return await _projectParticipateTableStorage.DeleteAsync(partitionKey, rowKey);
+        }
+    }
+}
