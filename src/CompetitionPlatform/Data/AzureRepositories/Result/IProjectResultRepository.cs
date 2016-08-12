f52a50c9a8280cd587c5ba7a221bdaf232c98cf2
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-08-12 09:53:30 +0400

    Change Result entity and repositories.

--- src/CompetitionPlatform/Data/AzureRepositories/Result/IProjectResultRepository.cs
+++ src/CompetitionPlatform/Data/AzureRepositories/Result/IProjectResultRepository.cs
@@ -1,11 +1,24 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace CompetitionPlatform.Data.AzureRepositories.Result
 {
+    public interface IProjectResultData
+    {
+        string ProjectId { get; set; }
+        string ParticipantId { get; set; }
+        string ParticipantFullName { get; set; }
+        string Link { get; set; }
+        DateTime Submitted { get; set; }
+        int Score { get; set; }
+    }
+
     public interface IProjectResultRepository
     {
-        Task InsertProjectResult(Stream stream, string resultId);
-        Task<Stream> GetProjectResult(string resultId);
+        Task<IProjectResultData> GetAsync(string projectId, string participantId);
+        Task<IEnumerable<IProjectResultData>> GetResultsAsync(string projectId);
+        Task SaveAsync(IProjectResultData resultData);
+        Task UpdateAsync(IProjectResultData resultData);
     }
 }
--- src/CompetitionPlatform/Data/AzureRepositories/Result/ProjectResultRepository.cs
+++ src/CompetitionPlatform/Data/AzureRepositories/Result/ProjectResultRepository.cs
@@ -1,28 +1,92 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
-using AzureStorage.Blobs;
+using AzureStorage.Tables;
+using Microsoft.WindowsAzure.Storage.Table;
 
 namespace CompetitionPlatform.Data.AzureRepositories.Result
 {
+    public class ProjectResultEntity : TableEntity, IProjectResultData
+    {
+        public static string GeneratePartitionKey(string projectId)
+        {
+            return projectId;
+        }
+
+        public static string GenerateRowKey(string participantId)
+        {
+            return participantId;
+        }
+
+        public string ProjectId { get; set; }
+        public string ParticipantId { get; set; }
+        public string ParticipantFullName { get; set; }
+        public string Link { get; set; }
+        public DateTime Submitted { get; set; }
+        public int Score { get; set; }
+
+        public static ProjectResultEntity Create(IProjectResultData src)
+        {
+            var result = new ProjectResultEntity
+            {
+                PartitionKey = GeneratePartitionKey(src.ProjectId),
+                RowKey = GeneratePartitionKey(src.ParticipantId),
+                ParticipantFullName = src.ParticipantFullName,
+                Link = src.Link,
+                Submitted = src.Submitted,
+                Score = src.Score
+            };
+
+            return result;
+        }
+
+        internal void Update(IProjectResultData src)
+        {
+            Link = src.Link;
+            Score = src.Score;
+        }
+    }
+
     public class ProjectResultRepository : IProjectResultRepository
     {
-        private readonly IAzureBlob _blobStorage;
+        private readonly IAzureTableStorage<ProjectResultEntity> _projectResultInfoTableStorage;
 
-        private const string ContainerName = "projectresults";
-
-        public ProjectResultRepository(IAzureBlob blobStorage)
+        public ProjectResultRepository(IAzureTableStorage<ProjectResultEntity> projectResultInfoTableStorage)
         {
-            _blobStorage = blobStorage;
+            _projectResultInfoTableStorage = projectResultInfoTableStorage;
         }
 
-        public async Task InsertProjectResult(Stream stream, string resultId)
+        public async Task<IProjectResultData> GetAsync(string projectId, string participantId)
         {
-            await _blobStorage.SaveBlobAsync(ContainerName, resultId, stream);
+            var partitionKey = ProjectResultEntity.GeneratePartitionKey(projectId);
+            var rowKey = ProjectResultEntity.GenerateRowKey(participantId);
+
+            return await _projectResultInfoTableStorage.GetDataAsync(partitionKey, rowKey);
         }
 
-        public async Task<Stream> GetProjectResult(string resultId)
+        public async Task<IEnumerable<IProjectResultData>> GetResultsAsync(string projectId)
         {
-            return await _blobStorage.GetAsync(ContainerName, resultId);
+            var partitionKey = ProjectResultEntity.GeneratePartitionKey(projectId);
+            return await _projectResultInfoTableStorage.GetDataAsync(partitionKey);
+        }
+
+        public async Task SaveAsync(IProjectResultData resultData)
+        {
+            var newEntity = ProjectResultEntity.Create(resultData);
+            await _projectResultInfoTableStorage.InsertAsync(newEntity);
+        }
+
+        public Task UpdateAsync(IProjectResultData resultData)
+        {
+            var partitionKey = ProjectResultEntity.GeneratePartitionKey(resultData.ProjectId);
+            var rowKey = ProjectResultEntity.GenerateRowKey(resultData.ParticipantId);
+
+            return _projectResultInfoTableStorage.ReplaceAsync(partitionKey, rowKey, itm =>
+            {
+                itm.Update(resultData);
+                return itm;
+            });
         }
     }
 }
