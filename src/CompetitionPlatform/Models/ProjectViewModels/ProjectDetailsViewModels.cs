ab28b9267508eb2de946773f9e77720adf7473d5
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-08-24 08:56:11 +0400

    Add status completion percent property to the model.

--- src/CompetitionPlatform/Models/ProjectViewModels/ProjectDetailsViewModels.cs
+++ src/CompetitionPlatform/Models/ProjectViewModels/ProjectDetailsViewModels.cs
@@ -83,6 +83,7 @@ namespace CompetitionPlatform.Models.ProjectViewModels
     public class ProjectDetailsStatusBarViewModel
     {
         public Status Status { get; set; }
+        public int StatusCompletionPercent { get; set; }
         public DateTime CompetitionRegistrationDeadline { get; set; }
         public DateTime ImplementationDeadline { get; set; }
         public DateTime VotingDeadline { get; set; }
