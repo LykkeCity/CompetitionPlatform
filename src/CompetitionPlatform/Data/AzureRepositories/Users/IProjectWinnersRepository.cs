f9c54541c2aa3613a10030ac53176f43a2bec8d4
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-08-30 22:23:24 +0400

    Add interface and repository for competition winners.

---
+++ src/CompetitionPlatform/Data/AzureRepositories/Users/IProjectWinnersRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CompetitionPlatform.Data.AzureRepositories.Users
+{
+    public interface IWinnerData
+    {
+        string ProjectId { get; set; }
+        string WinnerId { get; set; }
+        string FullName { get; set; }
+        int Place { get; set; }
+        string Result { get; set; }
+        int Votes { get; set; }
+        int Score { get; set; }
+    }
+
+    public interface IProjectWinnersRepository
+    {
+        Task<IWinnerData> GetAsync(string projectId, string userId);
+        Task<IEnumerable<IWinnerData>> GetWinnersAsync(string projectId);
+        Task SaveAsync(IWinnerData winnerData);
+    }
+}
---
+++ src/CompetitionPlatform/Data/AzureRepositories/Users/ProjectWinnersRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+using AzureStorage.Tables;
+
+namespace CompetitionPlatform.Data.AzureRepositories.Users
+{
+    public class WinnerEntity : TableEntity, IWinnerData
+    {
+        public static string GeneratePartitionKey(string projectId)
+        {
+            return projectId;
+        }
+
+        public static string GenerateRowKey(string userId)
+        {
+            return userId;
+        }
+
+        public static WinnerEntity Create(IWinnerData src)
+        {
+            var result = new WinnerEntity
+            {
+                PartitionKey = GeneratePartitionKey(src.ProjectId),
+                RowKey = GenerateRowKey(src.WinnerId),
+                FullName = src.FullName,
+                Place = src.Place,
+                Result = src.Result,
+                Votes = src.Votes,
+                Score = src.Score
+            };
+
+            return result;
+        }
+
+        public string ProjectId { get; set; }
+        public string WinnerId { get; set; }
+        public string FullName { get; set; }
+        public int Place { get; set; }
+        public string Result { get; set; }
+        public int Votes { get; set; }
+        public int Score { get; set; }
+    }
+
+    public class ProjectWinnersRepository : IProjectWinnersRepository
+    {
+        private readonly IAzureTableStorage<WinnerEntity> _winnersStorage;
+
+        public async Task<IWinnerData> GetAsync(string projectId, string userId)
+        {
+            var partitionKey = WinnerEntity.GeneratePartitionKey(projectId);
+            var rowKey = WinnerEntity.GenerateRowKey(userId);
+
+            return await _winnersStorage.GetDataAsync(partitionKey, rowKey);
+        }
+
+        public async Task<IEnumerable<IWinnerData>> GetWinnersAsync(string projectId)
+        {
+            var partitionKey = WinnerEntity.GeneratePartitionKey(projectId);
+
+            return await _winnersStorage.GetDataAsync(partitionKey);
+        }
+
+        public async Task SaveAsync(IWinnerData winnerData)
+        {
+            var newEntity = WinnerEntity.Create(winnerData);
+
+            await _winnersStorage.InsertAsync(newEntity);
+        }
+    }
+}
