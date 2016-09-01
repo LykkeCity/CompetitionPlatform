43718c31d6aaedda250db1e0df6145cf597f4d72
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-09-01 14:25:31 +0400

    Add interface and repository for User Roles ( Creator and Admin )

---
+++ src/CompetitionPlatform/Data/AzureRepositories/Users/IUserRolesRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompetitionPlatform.Data.AzureRepositories.Users
+{
+    public interface IUSerRoleData
+    {
+        string Role { get; set; }
+    }
+
+    public interface IUserRolesRepository
+    {
+        Task<IUSerRoleData> GetAsync(string userId);
+    }
+}
---
+++ src/CompetitionPlatform/Data/AzureRepositories/Users/UserRolesRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AzureStorage.Tables;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace CompetitionPlatform.Data.AzureRepositories.Users
+{
+    public class UserRoleEntity : TableEntity, IUSerRoleData
+    {
+        public static string GeneratePartitionKey()
+        {
+            return "Role";
+        }
+
+        public static string GenerateRowKey(string userId)
+        {
+            return userId;
+        }
+
+        public string Role { get; set; }
+    }
+
+    public class UserRolesRepository : IUserRolesRepository
+    {
+        private readonly IAzureTableStorage<UserRoleEntity> _userRoleTableStorage;
+
+        public UserRolesRepository(IAzureTableStorage<UserRoleEntity> userRoleTableStorage)
+        {
+            _userRoleTableStorage = userRoleTableStorage;
+        }
+
+        public async Task<IUSerRoleData> GetAsync(string userId)
+        {
+            var partitionKey = UserRoleEntity.GeneratePartitionKey();
+            var rowKey = UserRoleEntity.GenerateRowKey(userId);
+
+            return await _userRoleTableStorage.GetDataAsync(partitionKey, rowKey);
+        }
+    }
+}
