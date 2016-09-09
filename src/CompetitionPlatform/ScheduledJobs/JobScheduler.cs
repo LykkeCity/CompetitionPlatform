7925c8a6bfe630f752962ad8f406df457f2862f1
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-09-09 08:42:33 +0400

    Remove unnecessary using statements.

--- src/CompetitionPlatform/ScheduledJobs/JobScheduler.cs
+++ src/CompetitionPlatform/ScheduledJobs/JobScheduler.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Common.Log;
-using CompetitionPlatform.Data.AzureRepositories.Log;
+﻿using Common.Log;
 using Quartz;
 using Quartz.Impl;
 
