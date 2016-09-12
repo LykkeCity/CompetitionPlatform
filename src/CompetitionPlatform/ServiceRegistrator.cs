8db581079aaa2df8768b48d103e1f71d6c42b195
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-09-12 12:43:50 +0400

    Register winners service, fix typo in variable name.

--- src/CompetitionPlatform/ServiceRegistrator.cs
+++ src/CompetitionPlatform/ServiceRegistrator.cs
@@ -13,43 +13,43 @@ namespace CompetitionPlatform
 {
     public static class ServiceRegistrator
     {
-        public static void RegisterRepositories(this IServiceCollection services, string connstionString, ILog log)
+        public static void RegisterRepositories(this IServiceCollection services, string connectionString, ILog log)
         {
             services.AddSingleton<IAzureTableStorage<ProjectEntity>>(
-                   new AzureTableStorage<ProjectEntity>(connstionString, "Projects", log));
+                   new AzureTableStorage<ProjectEntity>(connectionString, "Projects", log));
 
             services.AddSingleton<IAzureBlob>(
-                new AzureBlobStorage(connstionString));
+                new AzureBlobStorage(connectionString));
 
             services.AddSingleton<IAzureTableStorage<UserEntity>>(
-                new AzureTableStorage<UserEntity>(connstionString, "Users", log));
+                new AzureTableStorage<UserEntity>(connectionString, "Users", log));
 
             services.AddSingleton<IAzureTableStorage<CommentEntity>>(
-                new AzureTableStorage<CommentEntity>(connstionString, "ProjectComments", log));
+                new AzureTableStorage<CommentEntity>(connectionString, "ProjectComments", log));
 
             services.AddSingleton<IAzureTableStorage<ProjectFileInfoEntity>>(
-                new AzureTableStorage<ProjectFileInfoEntity>(connstionString, "ProjectFilesInfo", log));
+                new AzureTableStorage<ProjectFileInfoEntity>(connectionString, "ProjectFilesInfo", log));
 
             services.AddSingleton<IAzureTableStorage<ProjectVoteEntity>>(
-                new AzureTableStorage<ProjectVoteEntity>(connstionString, "ProjectVotes", log));
+                new AzureTableStorage<ProjectVoteEntity>(connectionString, "ProjectVotes", log));
 
             services.AddSingleton<IAzureTableStorage<ProjectParticipateEntity>>(
-                new AzureTableStorage<ProjectParticipateEntity>(connstionString, "ProjectParticipants", log));
+                new AzureTableStorage<ProjectParticipateEntity>(connectionString, "ProjectParticipants", log));
 
             services.AddSingleton<IAzureTableStorage<ProjectResultEntity>>(
-                new AzureTableStorage<ProjectResultEntity>(connstionString, "ProjectResults", log));
+                new AzureTableStorage<ProjectResultEntity>(connectionString, "ProjectResults", log));
 
             services.AddSingleton<IAzureTableStorage<ProjectResultVoteEntity>>(
-                new AzureTableStorage<ProjectResultVoteEntity>(connstionString, "ProjectResultVotes", log));
+                new AzureTableStorage<ProjectResultVoteEntity>(connectionString, "ProjectResultVotes", log));
 
             services.AddSingleton<IAzureTableStorage<ProjectFollowEntity>>(
-                new AzureTableStorage<ProjectFollowEntity>(connstionString, "ProjectFollows", log));
+                new AzureTableStorage<ProjectFollowEntity>(connectionString, "ProjectFollows", log));
 
             services.AddSingleton<IAzureTableStorage<WinnerEntity>>(
-                new AzureTableStorage<WinnerEntity>(connstionString, "Winners", log));
+                new AzureTableStorage<WinnerEntity>(connectionString, "Winners", log));
 
             services.AddSingleton<IAzureTableStorage<UserRoleEntity>>(
-                new AzureTableStorage<UserRoleEntity>(connstionString, "UserRoles", log));
+                new AzureTableStorage<UserRoleEntity>(connectionString, "UserRoles", log));
 
             services.AddTransient<IProjectRepository, ProjectRepository>();
             services.AddTransient<IProjectFileRepository, ProjectFileRepository>();
@@ -64,6 +64,7 @@ namespace CompetitionPlatform
             services.AddTransient<IProjectFollowRepository, ProjectFollowRepository>();
             services.AddTransient<IProjectWinnersRepository, ProjectWinnersRepository>();
             services.AddTransient<IUserRolesRepository, UserRolesRepository>();
+            services.AddTransient<IProjectWinnersService, ProjectWinnersService>();
         }
 
         public static void RegisterLyykeServices(this IServiceCollection services)
