9973ba9a7e77b4bbb0d19330c9c36088b3c94108
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-09-12 12:42:36 +0400

    Rename class, add repositories and winners service.

---
+++ src/CompetitionPlatform/ScheduledJobs/ProjectStatusUpdaterJob.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using AzureStorage.Tables;
+using Common.Log;
+using CompetitionPlatform.Data.AzureRepositories.Project;
+using CompetitionPlatform.Data.AzureRepositories.Result;
+using CompetitionPlatform.Data.AzureRepositories.Users;
+using CompetitionPlatform.Services;
+using Quartz;
+
+namespace CompetitionPlatform.ScheduledJobs
+{
+    public class ProjectStatusUpdaterJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            var dataMap = context.JobDetail.JobDataMap;
+
+            var connectionString = dataMap.GetString("connectionString");
+            var log = (ILog)dataMap["log"];
+
+            var projectRepository =
+                new ProjectRepository(new AzureTableStorage<ProjectEntity>(connectionString, "Projects", log));
+
+            var resultRepository =
+                new ProjectResultRepository(new AzureTableStorage<ProjectResultEntity>(connectionString, "ProjectResults", log));
+
+            var winnersRepository =
+                new ProjectWinnersRepository(new AzureTableStorage<WinnerEntity>(connectionString, "Winners", log));
+
+            var winnersService = new ProjectWinnersService(projectRepository, resultRepository, winnersRepository);
+
+            var statusUpdater = new StatusUpdater(projectRepository, winnersService);
+
+            await statusUpdater.UpdateProjectStatuses();
+        }
+    }
+}
