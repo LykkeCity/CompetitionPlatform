baf68574f773748cf410eb8dba2694388f6489cf
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-09-20 11:30:34 +0400

    Show appropriate icons for project files, Add icons and a helper class to identify content types.

---
+++ src/CompetitionPlatform/Helpers/ContentTypesHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace CompetitionPlatform.Helpers
+{
+    public static class ContentTypesHelper
+    {
+        public const string Pdf = "application/pdf";
+
+        public const string Jpg = "image/jpeg";
+
+        public const string Doc = "application/msword";
+
+        public const string Docx = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+
+        public const string Xls = "application/vnd.ms-excel";
+
+        public const string Xlsx = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+    }
+}
