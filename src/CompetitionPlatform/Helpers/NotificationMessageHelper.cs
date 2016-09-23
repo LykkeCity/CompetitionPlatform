dd165290da89b9a622b2b45cf27c441e8faa2f96
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-09-23 12:12:30 +0400

    Add a helper class to compose broadcasted messages.

---
+++ src/CompetitionPlatform/Helpers/NotificationMessageHelper.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+
+namespace CompetitionPlatform.Helpers
+{
+    public static class NotificationMessageHelper
+    {
+        public static string ProjectCreatedMessage(string userEmail, string userName, string projectName)
+        {
+            var messageData = new MessageData
+            {
+                Subject = "New Project Created - " + projectName,
+                Text = "User " + userName + " (" + userEmail + ") " + "Has Created a new project - " + projectName
+            };
+
+            var data = new Data
+            {
+                BroadcastGroup = 600,
+                MessageData = messageData
+            };
+
+            var plainTextBroadCast = new PlainTextBroadcast { Data = data };
+
+            return "PlainTextBroadcast:" + JsonConvert.SerializeObject(plainTextBroadCast);
+        }
+    }
+
+    public class PlainTextBroadcast
+    {
+        public Data Data { get; set; }
+    }
+
+    public class Data
+    {
+        public int BroadcastGroup { get; set; }
+        public MessageData MessageData { get; set; }
+    }
+
+    public class MessageData
+    {
+        public string Subject { get; set; }
+        public string Text { get; set; }
+    }
+}
