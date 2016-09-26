6b5437a128ba7896fcb5317e6bf849e09bced314
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-09-26 11:17:40 +0400

    Remove unnecessary using statements.

--- src/CompetitionPlatform/Data/AzureRepositories/Settings/BaseSettings.cs
+++ src/CompetitionPlatform/Data/AzureRepositories/Settings/BaseSettings.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace CompetitionPlatform.Data.AzureRepositories.Settings
+﻿namespace CompetitionPlatform.Data.AzureRepositories.Settings
 {
     public class BaseSettings
     {
