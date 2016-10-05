8c3a3e101377c34109dbacf538a61b627a3775d1
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-10-05 11:58:03 +0400

    Add a repository to track which users received registration messages, and Initiative messages on project follow.

---
+++ src/CompetitionPlatform/Data/AzureRepositories/Users/IMailSentRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CompetitionPlatform.Data.AzureRepositories.Users
+{
+    public interface IMailSentData
+    {
+        string UserId { get; set; }
+        string ProjectId { get; set; }
+    }
+
+    public interface IMailSentRepository
+    {
+        Task SaveRegisterAsync(string userId);
+        Task SaveFollowAsync(string userId, string projectId);
+        Task<IMailSentData> GetRegisterAsync(string userId);
+        Task<IEnumerable<IMailSentData>> GetFollowAsync();
+    }
+}
---
+++ src/CompetitionPlatform/Data/AzureRepositories/Users/MailSentRepository.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AzureStorage.Tables;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace CompetitionPlatform.Data.AzureRepositories.Users
+{
+    public class MailSentEntity : TableEntity, IMailSentData
+    {
+        public static string GeneratePartitionKey(string partition)
+        {
+            return partition;
+        }
+
+        public static string GenerateRowKey(string userId)
+        {
+            return userId;
+        }
+
+        public static MailSentEntity Create(string partition, string userId, string projectId = "")
+        {
+            var result = new MailSentEntity
+            {
+                PartitionKey = GeneratePartitionKey(partition),
+                RowKey = GenerateRowKey(userId),
+                UserId = userId
+            };
+
+            return result;
+        }
+
+        public string UserId { get; set; }
+        public string ProjectId { get; set; }
+    }
+
+    public class MailSentRepository : IMailSentRepository
+    {
+        private readonly IAzureTableStorage<MailSentEntity> _mailSentStorage;
+
+        public MailSentRepository(IAzureTableStorage<MailSentEntity> mailSentStorage)
+        {
+            _mailSentStorage = mailSentStorage;
+        }
+
+        public async Task SaveRegisterAsync(string userId)
+        {
+            var newEntity = MailSentEntity.Create("Register", userId);
+            await _mailSentStorage.InsertAsync(newEntity);
+        }
+
+        public async Task SaveFollowAsync(string userId, string projectId)
+        {
+            var newEntity = MailSentEntity.Create("Follow", userId, projectId);
+            await _mailSentStorage.InsertAsync(newEntity);
+        }
+
+        public async Task<IMailSentData> GetRegisterAsync(string userId)
+        {
+            var partitionKey = MailSentEntity.GeneratePartitionKey("Register");
+            var rowKey = MailSentEntity.GenerateRowKey(userId);
+
+            return await _mailSentStorage.GetDataAsync(partitionKey, rowKey);
+        }
+
+        public async Task<IEnumerable<IMailSentData>> GetFollowAsync()
+        {
+            var partitionKey = MailSentEntity.GeneratePartitionKey("Follow");
+
+            return await _mailSentStorage.GetDataAsync(partitionKey);
+        }
+    }
+}
