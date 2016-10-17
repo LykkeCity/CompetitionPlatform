e258b1bce57ee9d280a874653dd713a1ab7c745b
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-10-17 07:43:21 +0400

    Catch an event when User logs in the application.

--- src/CompetitionPlatform/Authentication/CompPlatformAuthenticationEvents.cs
+++ src/CompetitionPlatform/Authentication/CompPlatformAuthenticationEvents.cs
@@ -1,4 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AzureStorage.Tables;
+using Common.Log;
+using CompetitionPlatform.Data.AzureRepositories.Users;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 
@@ -6,12 +11,28 @@ namespace CompetitionPlatform.Authentication
 {
     public class CompPlatformAuthenticationEvents : OpenIdConnectEvents
     {
+        private readonly IProjectFollowRepository _projectFollowRepository;
+
+        public CompPlatformAuthenticationEvents(string connString, ILog log)
+        {
+            _projectFollowRepository = new ProjectFollowRepository(new AzureTableStorage<ProjectFollowEntity>(connString, "ProjectFollows", log));
+        }
+
         public override Task RemoteFailure(FailureContext context)
         {
             context.HandleResponse();
             context.Response.Redirect("/Home/AuthenticationFailed");
 
             return Task.FromResult(0);
+        }
+
+        public override async Task TokenValidated(TokenValidatedContext context)
+        {
+            var email = context.Ticket.Principal.Claims.Where(c => c.Type == ClaimTypes.Email)
+                   .Select(c => c.Value).SingleOrDefault();
+
+            var follows = await _projectFollowRepository.GetFollowAsync();
+            await base.TokenValidated(context);
         }
     }
 }
