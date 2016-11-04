7369b3000f8fa4a5d4326a75cc8e25a7bb0fdda3
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-11-04 14:42:53 +0400

    Add models for Registration and Archive emails.

--- src/CompetitionPlatform/Models/NotificationModels.cs
+++ src/CompetitionPlatform/Models/NotificationModels.cs
@@ -2,6 +2,11 @@
 
 namespace CompetitionPlatform.Models
 {
+    public class RegistrationEmailModel
+    {
+        public string FirstName { get; set; }
+    }
+
     public class Initiative
     {
         public string FirstName { get; set; }
@@ -55,5 +60,38 @@ namespace CompetitionPlatform.Models
         public string ProjectDescription { get; set; }
         public double ProjectFirstPrize { get; set; }
         public double ProjectSecondPrize { get; set; }
+    }
+
+    public class Archive
+    {
+        public string FirstName { get; set; }
+        public string ProjectId { get; set; }
+        public DateTime ProjectCreatedDate { get; set; }
+        public string ProjectAuthorName { get; set; }
+        public string ProjectName { get; set; }
+        public string ProjectStatus { get; set; }
+        public string ProjectDescription { get; set; }
+        public double ProjectFirstPrize { get; set; }
+        public double ProjectSecondPrize { get; set; }
+        public int ParticipantCount { get; set; }
+        public int ResultCount { get; set; }
+        public int WinnerCount { get; set; }
+        public int Duration { get; set; }
+        public string FirstPlaceWinnerName { get; set; }
+        public string FirstPlaceWinnerId { get; set; }
+        public int FirstPlaceWinnerScore { get; set; }
+        public int FirstPlaceWinnerVotes { get; set; }
+        public string SecondPlaceWinnerName { get; set; }
+        public string SecondPlaceWinnerId { get; set; }
+        public int SecondPlaceWinnerScore { get; set; }
+        public int SecondPlaceWinnerVotes { get; set; }
+        public string ThirdPlaceWinnerName { get; set; }
+        public string ThirdPlaceWinnerId { get; set; }
+        public int ThirdPlaceWinnerScore { get; set; }
+        public int ThirdPlaceWinnerVotes { get; set; }
+        public string FourthPlaceWinnerName { get; set; }
+        public string FourthPlaceWinnerId { get; set; }
+        public int FourthPlaceWinnerScore { get; set; }
+        public int FourthPlaceWinnerVotes { get; set; }
     }
 }
