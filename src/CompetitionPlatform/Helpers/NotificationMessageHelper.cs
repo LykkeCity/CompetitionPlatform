0c08ad7901b4ad384d0c422a85eb54082aaca33f
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-11-04 14:41:06 +0400

    Change email sender to Lykke Stream, add functions to generate Archive and Registration emails.

--- src/CompetitionPlatform/Helpers/NotificationMessageHelper.cs
+++ src/CompetitionPlatform/Helpers/NotificationMessageHelper.cs
@@ -1,4 +1,6 @@
-﻿using CompetitionPlatform.Data.AzureRepositories.Project;
+﻿using System.Collections.Generic;
+using System.Linq;
+using CompetitionPlatform.Data.AzureRepositories.Project;
 using CompetitionPlatform.Data.AzureRepositories.Users;
 using CompetitionPlatform.Models;
 using Newtonsoft.Json;
@@ -8,6 +10,37 @@ namespace CompetitionPlatform.Helpers
     public static class NotificationMessageHelper
     {
         private const string MailType = "CompetitionPlatformMail:";
+        private const string EmailSender = "Lykke Streams";
+
+        public static string GenerateRegistrationMessage(string firstName, string email)
+        {
+            var model = new RegistrationEmailModel
+            {
+                FirstName = firstName
+            };
+
+            var messageData = new RegistrationEmailMessageData
+            {
+                Subject = "Registration",
+                Sender = EmailSender,
+                Model = model
+            };
+
+            var data = new RegistrationEmailData
+            {
+                EmailAddress = email,
+                MessageData = messageData
+            };
+
+            var registrationEmail = new RegistrationEmail
+            {
+                Data = data
+            };
+
+            var message = JsonConvert.SerializeObject(registrationEmail);
+
+            return MailType + message;
+        }
 
         public static string GenerateInitiativeMessage(IProjectData project, string userName, string email)
         {
@@ -27,7 +60,7 @@ namespace CompetitionPlatform.Helpers
             var messageData = new InitiativeEmailMessageData
             {
                 Subject = "Initiative",
-                Sender = "Competition Platform",
+                Sender = EmailSender,
                 Model = model
             };
 
@@ -66,7 +99,7 @@ namespace CompetitionPlatform.Helpers
             var messageData = new CompetitionEmailMessageData
             {
                 Subject = "Competition",
-                Sender = "Competition Platform",
+                Sender = EmailSender,
                 Model = model
             };
 
@@ -105,7 +138,7 @@ namespace CompetitionPlatform.Helpers
             var messageData = new ImplementationEmailMessageData
             {
                 Subject = templateType,
-                Sender = "Competition Platform",
+                Sender = EmailSender,
                 Model = model
             };
 
@@ -144,7 +177,7 @@ namespace CompetitionPlatform.Helpers
             var messageData = new VotingEmailMessageData
             {
                 Subject = "Voting",
-                Sender = "Competition Platform",
+                Sender = EmailSender,
                 Model = model
             };
 
@@ -160,6 +193,120 @@ namespace CompetitionPlatform.Helpers
             };
 
             var message = JsonConvert.SerializeObject(votingEmail);
+
+            return MailType + message;
+        }
+
+        public static string GenerateArchiveMessage(IProjectData project, IProjectFollowData follower, int participantsCount, int resultsCount, IEnumerable<IWinnerData> winners)
+        {
+            var duration = (project.VotingDeadline - project.Created).Days;
+
+            var firstPlaceWinnerName = "";
+            var firstPlaceWinnerId = "";
+            var firstPlaceWinnerScore = 0;
+            var firstPlaceWinnerVotes = 0;
+
+            var secondPlaceWinnerName = "";
+            var secondPlaceWinnerId = "";
+            var secondPlaceWinnerScore = 0;
+            var secondPlaceWinnerVotes = 0;
+
+            var thirdPlaceWinnerName = "";
+            var thirdPlaceWinnerId = "";
+            var thirdPlaceWinnerScore = 0;
+            var thirdPlaceWinnerVotes = 0;
+
+            var fourthPlaceWinnerName = "";
+            var fourthPlaceWinnerId = "";
+            var fourthPlaceWinnerScore = 0;
+            var fourthPlaceWinnerVotes = 0;
+
+            var winnersData = winners as IWinnerData[] ?? winners.ToArray();
+
+            for (int i = 1; i <= winnersData.Count(); i++)
+            {
+                if (i == 1)
+                {
+                    firstPlaceWinnerName = winnersData[0].FullName;
+                    firstPlaceWinnerId = winnersData[0].WinnerId;
+                    firstPlaceWinnerScore = winnersData[0].Score;
+                    firstPlaceWinnerVotes = winnersData[0].Votes;
+                }
+                if (i == 2)
+                {
+                    secondPlaceWinnerName = winnersData[1].FullName;
+                    secondPlaceWinnerId = winnersData[1].WinnerId;
+                    secondPlaceWinnerScore = winnersData[1].Score;
+                    secondPlaceWinnerVotes = winnersData[1].Votes;
+                }
+                if (i == 3)
+                {
+                    thirdPlaceWinnerName = winnersData[2].FullName;
+                    thirdPlaceWinnerId = winnersData[2].WinnerId;
+                    thirdPlaceWinnerScore = winnersData[2].Score;
+                    thirdPlaceWinnerVotes = winnersData[2].Votes;
+                }
+                if (i == 4)
+                {
+                    fourthPlaceWinnerName = winnersData[3].FullName;
+                    fourthPlaceWinnerId = winnersData[3].WinnerId;
+                    fourthPlaceWinnerScore = winnersData[3].Score;
+                    fourthPlaceWinnerVotes = winnersData[3].Votes;
+                }
+            }
+
+            var model = new Archive
+            {
+                FirstName = GetFirstNameFromFullName(follower.FullName),
+                ProjectId = project.Id,
+                ProjectCreatedDate = project.Created,
+                ProjectAuthorName = project.AuthorFullName,
+                ProjectName = project.Name,
+                ProjectStatus = Status.Archive.ToString(),
+                ProjectDescription = project.Description,
+                ProjectFirstPrize = project.BudgetFirstPlace,
+                ProjectSecondPrize = project.BudgetSecondPlace ?? 0,
+                ParticipantCount = participantsCount,
+                ResultCount = resultsCount,
+                WinnerCount = winnersData.Count(),
+                Duration = duration,
+                FirstPlaceWinnerName = firstPlaceWinnerName,
+                FirstPlaceWinnerId = firstPlaceWinnerId,
+                FirstPlaceWinnerScore = firstPlaceWinnerScore,
+                FirstPlaceWinnerVotes = firstPlaceWinnerVotes,
+                SecondPlaceWinnerName = secondPlaceWinnerName,
+                SecondPlaceWinnerId = secondPlaceWinnerId,
+                SecondPlaceWinnerScore = secondPlaceWinnerScore,
+                SecondPlaceWinnerVotes = secondPlaceWinnerVotes,
+                ThirdPlaceWinnerName = thirdPlaceWinnerName,
+                ThirdPlaceWinnerId = thirdPlaceWinnerId,
+                ThirdPlaceWinnerScore = thirdPlaceWinnerScore,
+                ThirdPlaceWinnerVotes = thirdPlaceWinnerVotes,
+                FourthPlaceWinnerName = fourthPlaceWinnerName,
+                FourthPlaceWinnerId = fourthPlaceWinnerId,
+                FourthPlaceWinnerScore = fourthPlaceWinnerScore,
+                FourthPlaceWinnerVotes = fourthPlaceWinnerVotes
+            };
+
+            var messageData = new ArchiveEmailMessageData
+            {
+                Subject = "Archive",
+                Sender = EmailSender,
+                Model = model
+            };
+
+            var data = new ArchiveEmailData
+            {
+                EmailAddress = follower.UserId,
+                MessageData = messageData
+            };
+
+            var archiveEmail = new ArchiveEmail
+            {
+                Data = data
+            };
+
+            var message = JsonConvert.SerializeObject(archiveEmail);
 
             return MailType + message;
         }
@@ -233,6 +380,24 @@ namespace CompetitionPlatform.Helpers
         public string Text { get; set; }
     }
 
+    public class RegistrationEmail
+    {
+        public RegistrationEmailData Data { get; set; }
+    }
+
+    public class RegistrationEmailData
+    {
+        public string EmailAddress { get; set; }
+        public RegistrationEmailMessageData MessageData { get; set; }
+    }
+
+    public class RegistrationEmailMessageData
+    {
+        public string Sender { get; set; }
+        public string Subject { get; set; }
+        public RegistrationEmailModel Model { get; set; }
+    }
+
     public class CompetitionEmail
     {
         public CompetitionEmailData Data { get; set; }
@@ -303,5 +468,23 @@ namespace CompetitionPlatform.Helpers
         public string Sender { get; set; }
         public string Subject { get; set; }
         public Voting Model { get; set; }
+    }
+
+    public class ArchiveEmail
+    {
+        public ArchiveEmailData Data { get; set; }
+    }
+
+    public class ArchiveEmailData
+    {
+        public string EmailAddress { get; set; }
+        public ArchiveEmailMessageData MessageData { get; set; }
+    }
+
+    public class ArchiveEmailMessageData
+    {
+        public string Sender { get; set; }
+        public string Subject { get; set; }
+        public Archive Model { get; set; }
     }
 }
