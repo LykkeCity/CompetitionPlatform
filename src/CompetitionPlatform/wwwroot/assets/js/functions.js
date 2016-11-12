96c3249f61f6ae8a37fb9925c68182346310c94f
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-11-12 23:35:07 +0400

    Add new styles and images.

---
+++ src/CompetitionPlatform/wwwroot/assets/js/functions.js
@@ -0,0 +1,89 @@
+// CODE ========================
+
+var wH = $(window).height(),
+    wW = $(window).width(),
+    ua = navigator.userAgent,
+    touchendOrClick = (ua.match(/iPad|iPhone|iPad/i)) ? "touchend" : "click",
+
+    deviceAgent = navigator.userAgent.toLowerCase(),
+    isMobile = deviceAgent.match(/(iphone|ipod|ipad)/);
+
+FastClick.attach(document.body);
+
+$(window).resize(function() {
+  $('.content').css({
+    paddingTop: $('.header').outerHeight()
+  });
+
+  $('.new_page').css({
+    paddingBottom: $('footer').outerHeight()
+  })
+}).trigger('resize');
+
+// Tel
+if (!isMobile) {
+  $('body').on('click', 'a[href^="tel:"]', function() {
+      $(this).attr('href',
+          $(this).attr('href').replace(/^tel:/, 'callto:'));
+  });
+}
+
+$(function() {
+
+  $('.scrollToAnchor').on('click', function(e) {
+    e.preventDefault();
+    var target = $(this).attr('href');
+
+    $('html,body').animate({
+      scrollTop: $(target).offset().top - 30
+    }, 1000);
+
+  });
+});
+
+
+$(function() {
+
+  $('#navbar-collapse')
+      .on('click', function(e) {
+        $('body').toggleClass('menu-collapsed');
+      });
+
+});
+
+$(function() {
+  //caches a jQuery object containing the header element
+  var header = $(".header");
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 10) {
+      header.addClass("fixed");
+    } else {
+      header.removeClass("fixed")
+    }
+  });
+});
+
+$('[data-control="select"] ._value').text($(this).siblings('select').val());
+$('[data-control="select"] select').on('change', function() {
+  $(this).siblings('.select__value').find('._value').text(this.value);
+});
+
+$('.action_follow').on('click', function() {
+  $(this).toggleClass('active')
+})
+
+$(window).scroll(function() {
+  if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+    $('body').find('#launcher').css({
+      bottom: 100
+    })
+  }
+
+  else {
+    $('body').find('#launcher').css({
+      bottom: 0
+    })
+  }
+});
