b03522ecf31925c2ba87625f3e918b2e157a8eba
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-11-14 16:20:04 +0400

    Add a repository for User Feedback.

---
+++ src/CompetitionPlatform/Data/AzureRepositories/Users/IUserFeedbackRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CompetitionPlatform.Data.AzureRepositories.Users
+{
+    public interface IUserFeedbackData
+    {
+        string Email { get; set; }
+        string Name { get; set; }
+        string Feedback { get; set; }
+        DateTime Created { get; set; }
+    }
+
+    public interface IUserFeedbackRepository
+    {
+        Task SaveAsync(IUserFeedbackData feedbackData);
+        Task<IEnumerable<IUserFeedbackData>> GetFeedbacksAsync();
+    }
+}
---
+++ src/CompetitionPlatform/Data/AzureRepositories/Users/UserFeedbackRepository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AzureStorage.Tables;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace CompetitionPlatform.Data.AzureRepositories.Users
+{
+    public class UserFeedbackEntity : TableEntity, IUserFeedbackData
+    {
+        public static string GeneratePartitionKey()
+        {
+            return "Feedback";
+        }
+
+        public static string GenerateRowKey()
+        {
+            return Guid.NewGuid().ToString("N");
+        }
+
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Feedback { get; set; }
+        public DateTime Created { get; set; }
+
+        public static UserFeedbackEntity Create(IUserFeedbackData src)
+        {
+            var result = new UserFeedbackEntity
+            {
+                PartitionKey = GeneratePartitionKey(),
+                RowKey = GenerateRowKey(),
+                Email = src.Email,
+                Name = src.Name,
+                Feedback = src.Feedback,
+                Created = src.Created
+            };
+
+            return result;
+        }
+    }
+
+    public class UserFeedbackRepository : IUserFeedbackRepository
+    {
+        private readonly IAzureTableStorage<UserFeedbackEntity> _userFeedbackTableStorage;
+
+        public UserFeedbackRepository(IAzureTableStorage<UserFeedbackEntity> userFeedbackTableStorage)
+        {
+            _userFeedbackTableStorage = userFeedbackTableStorage;
+        }
+
+        public async Task SaveAsync(IUserFeedbackData feedbackData)
+        {
+            var newEntity = UserFeedbackEntity.Create(feedbackData);
+            await _userFeedbackTableStorage.InsertAsync(newEntity);
+        }
+
+        public async Task<IEnumerable<IUserFeedbackData>> GetFeedbacksAsync()
+        {
+            var partitionKey = UserFeedbackEntity.GeneratePartitionKey();
+
+            return await _userFeedbackTableStorage.GetDataAsync(partitionKey);
+        }
+    }
+}
