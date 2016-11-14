abad4c6b9f0d6220f4c1d0bcff2ce27463003951
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-11-14 16:19:40 +0400

    Add models for User feedback.

---
+++ src/CompetitionPlatform/Models/FeedbackViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using CompetitionPlatform.Data.AzureRepositories.Users;
+
+namespace CompetitionPlatform.Models
+{
+    public class FeedbackViewModel : IUserFeedbackData
+    {
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Your name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Your feedback")]
+        public string Feedback { get; set; }
+
+        public DateTime Created { get; set; }
+    }
+
+    public class FeedbackListViewModel
+    {
+        public IEnumerable<IUserFeedbackData> FeedbackList { get; set; }
+    }
+}
