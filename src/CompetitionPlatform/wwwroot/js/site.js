c792a6864dc705ecf158c70f85650b44e6bd1520
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2017-01-24 11:18:03 +0400

    Remove Comments, Refactor code.

--- src/CompetitionPlatform/wwwroot/js/site.js
+++ src/CompetitionPlatform/wwwroot/js/site.js
@@ -219,7 +219,7 @@
 
             var today = new Date();
             var dd = today.getDate();
-            var mm = today.getMonth() + 1; 
+            var mm = today.getMonth() + 1;
 
             var yyyy = today.getFullYear();
             if (dd < 10) { dd = '0' + dd } if (mm < 10) { mm = '0' + mm } today = yyyy + '-' + mm + '-' + dd;
@@ -250,7 +250,7 @@
             $('#registrationDeadlineDatepicker').attr('value', getTodayFormatted());
 
             $('#registrationDeadlineDatepicker').hide();
-            $('.registrationDeadlineDatepickerValidation').hide(); 
+            $('.registrationDeadlineDatepickerValidation').hide();
         }
     });
 
