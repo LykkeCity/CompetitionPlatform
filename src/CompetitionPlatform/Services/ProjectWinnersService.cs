eb713cc44a7bc6366ecf32d3bb211dafb9a7efd3
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2017-02-08 10:21:55 +0400

    Remove comments.

--- src/CompetitionPlatform/Services/ProjectWinnersService.cs
+++ src/CompetitionPlatform/Services/ProjectWinnersService.cs
@@ -30,58 +30,6 @@ namespace CompetitionPlatform.Services
             _resultVoteRepository = resultVoteRepository;
         }
 
-        //public async Task SaveWinners(string projectId)
-        //{
-        //    var project = await _projectRepository.GetAsync(projectId);
-
-        //    var results = await _resultRepository.GetResultsAsync(projectId);
-
-        //    results = results.OrderByDescending(x => x.Votes).ThenByDescending(x => x.Score);
-
-        //    var resultDatas = results as IList<IProjectResultData> ?? results.ToList();
-
-        //    var firstPlaceResult = resultDatas.FirstOrDefault();
-
-        //    if (firstPlaceResult != null)
-        //    {
-        //        var firstPlaceWinner = new WinnerViewModel
-        //        {
-        //            ProjectId = firstPlaceResult.ProjectId,
-        //            WinnerId = firstPlaceResult.ParticipantId,
-        //            FullName = firstPlaceResult.ParticipantFullName,
-        //            Result = firstPlaceResult.Link,
-        //            Votes = firstPlaceResult.Votes,
-        //            Score = firstPlaceResult.Score,
-        //            Place = 1,
-        //            Budget = project.BudgetFirstPlace
-        //        };
-
-        //        await _winnersRepository.SaveAsync(firstPlaceWinner);
-        //    }
-
-        //    if (project.BudgetSecondPlace != null)
-        //    {
-        //        var secondPlaceResults = resultDatas.Skip(1).Take(3);
-
-        //        foreach (var result in secondPlaceResults)
-        //        {
-        //            var winner = new WinnerViewModel
-        //            {
-        //                ProjectId = result.ProjectId,
-        //                WinnerId = result.ParticipantId,
-        //                FullName = result.ParticipantFullName,
-        //                Result = result.Link,
-        //                Votes = result.Votes,
-        //                Score = result.Score,
-        //                Place = 2,
-        //                Budget = project.BudgetSecondPlace
-        //            };
-
-        //            await _winnersRepository.SaveAsync(winner);
-        //        }
-        //    }
-        //}
-
         public async Task SaveWinners(string projectId)
         {
             var project = await _projectRepository.GetAsync(projectId);
