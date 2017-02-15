a841b83e54b884aa9cf26e7b06cafcfb89fc13fa
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2017-02-15 14:34:50 +0400

    Remove automatic tab switching. WIP.

--- src/CompetitionPlatform/wwwroot/js/site.js
+++ src/CompetitionPlatform/wwwroot/js/site.js
@@ -265,20 +265,20 @@
         return today;
     }
 
-    $('#projectDetailsTabs a').click(function (e) {
-        e.preventDefault();
-        $(this).tab('show');
-    });
+    //$('#projectDetailsTabs a').click(function (e) {
+    //    e.preventDefault();
+    //    $(this).tab('show');
+    //});
 
-    // store the currently selected tab in the hash value
-    $("ul.nav-tabs > li > a").on("shown.bs.tab", function (e) {
-        var id = $(e.target).attr("href").substr(1);
-        window.location.hash = id;
-    });
+    //// store the currently selected tab in the hash value
+    //$("ul.nav-tabs > li > a").on("shown.bs.tab", function (e) {
+    //    var id = $(e.target).attr("href").substr(1);
+    //    window.location.hash = id;
+    //});
 
-    // on load of the page: switch to the currently selected tab
-    var hash = window.location.hash;
-    $('#projectDetailsTabs a[href="' + hash + '"]').tab('show');
+    //// on load of the page: switch to the currently selected tab
+    //var hash = window.location.hash;
+    //$('#projectDetailsTabs a[href="' + hash + '"]').tab('show');
 
     $('#datetimepicker1').datetimepicker();
 });
