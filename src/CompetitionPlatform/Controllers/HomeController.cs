475985d2d2f4c44763dc4b73120d523e63862382
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2017-03-24 13:42:51 +0400

    Order main page projects by status and then by budget.

--- src/CompetitionPlatform/Controllers/HomeController.cs
+++ src/CompetitionPlatform/Controllers/HomeController.cs
@@ -50,6 +50,7 @@ namespace CompetitionPlatform.Controllers
         public async Task<IActionResult> Index()
         {
             var viewModel = await GetProjectListViewModel(currentProjects: true);
+            viewModel.Projects = viewModel.Projects.OrderByDescending(x => x.Status).ThenBy(x => x.BudgetFirstPlace);
             return View(viewModel);
         }
 
