94bacce2f7b418159506cc34675a7ce499f64a25
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2017-05-05 09:26:33 +0400

    Add a method to get settings from a url.

--- src/CompetitionPlatform/Data/AzureRepositories/Settings/GeneralSettingsReader.cs
+++ src/CompetitionPlatform/Data/AzureRepositories/Settings/GeneralSettingsReader.cs
@@ -1,6 +1,8 @@
-﻿using System.Text;
+﻿using System.Net.Http;
+using System.Text;
 using AzureStorage.Blob;
 using Common;
+using Newtonsoft.Json;
 
 namespace CompetitionPlatform.Data.AzureRepositories.Settings
 {
@@ -13,6 +15,14 @@ namespace CompetitionPlatform.Data.AzureRepositories.Settings
             var str = Encoding.UTF8.GetString(settingsData);
 
             return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(str);
+        }
+
+        public static T ReadGeneralSettingsFromUrl<T>(string settingsUrl)
+        {
+            var httpClient = new HttpClient();
+            var settingsString = httpClient.GetStringAsync(settingsUrl).Result;
+            var serviceBusSettings = JsonConvert.DeserializeObject<T>(settingsString);
+            return serviceBusSettings;
         }
     }
 }
