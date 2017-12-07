b835a6566de9350c4a097c6f5b4864d86b107797
Justin L <hello@levinsonjust.in>
2017-12-07 17:02:01 -0500

    Refactored ProjectDetailsViewModels into separate classes

---
+++ src/CompetitionPlatform/Models/ProjectViewModels/ResultVoteViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CompetitionPlatform.Data.AzureRepositories.Vote;
+
+namespace CompetitionPlatform.Models.ProjectViewModels
+{
+    public class ResultVoteViewModel : IProjectResultVoteData
+    {
+        public string ProjectId { get; set; }
+        public string ParticipantId { get; set; }
+        public string VoterUserId { get; set; }
+        public string UserAgent { get; set; }
+        public string Type { get; set; }
+    }
+}
