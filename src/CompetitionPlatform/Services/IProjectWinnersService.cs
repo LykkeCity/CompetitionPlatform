f3674f4ef322e2d5848ffb5d9a80042026df4b48
Lasha Skhirtladze <lasha.skhirtladze@lykke.com>
2018-04-16 15:56:18 +0400

    COMPET-232 add custom budget

--- src/CompetitionPlatform/Services/IProjectWinnersService.cs
+++ src/CompetitionPlatform/Services/IProjectWinnersService.cs
@@ -7,5 +7,6 @@ namespace CompetitionPlatform.Services
     public interface IProjectWinnersService
     {
         Task SaveWinners(string projectId, IEnumerable<WinnerViewModel> winners = null);
+        Task SaveCustomWinners(string projectId, IEnumerable<WinnerViewModel> winners);
     }
 }
