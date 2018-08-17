24041b8cb7b96d8a12c2910f6ee7bff38fe92b5e
Lasha Skhirtladze <lasha.skhirtladze@lykke.com>
2018-08-17 10:36:31 +0400

    temp

--- src/CompetitionPlatform/Startup.cs
+++ src/CompetitionPlatform/Startup.cs
@@ -82,8 +82,8 @@ namespace CompetitionPlatform
                     options.Authority = _settings.LykkeStreams.Authentication.Authority;
                     options.ClientId = _settings.LykkeStreams.Authentication.ClientId;
                     options.ClientSecret = _settings.LykkeStreams.Authentication.ClientSecret;
-                    options.RequireHttpsMetadata = true;
-                    options.SaveTokens = false;
+                    options.RequireHttpsMetadata = false;
+                    options.SaveTokens = true;
                     options.CallbackPath = "/auth";
                     options.ResponseType = OpenIdConnectResponseType.Code;
                     options.Events = new CompPlatformAuthenticationEvents(Log, HostingEnvironment,
